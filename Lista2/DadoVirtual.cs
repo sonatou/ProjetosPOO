c40215b47dcabfda33c86fa407cfc982cd60cb6e
Allan Almeida <92676069+sonatou@users.noreply.github.com>
2023-09-21 20:16:03 -0300

    Primeira atividade Lista2

---
+++ Lista2/DadoVirtual.cs
@@ -0,0 +1,19 @@
+﻿namespace Lista2
+{
+    internal class DadoVirtual
+    {
+        private int numFaces;
+        private Random random;
+
+        public DadoVirtual(int numFaces)
+        {
+            this.numFaces = numFaces;
+            random = new Random();
+        }
+
+        public int Rolar()
+        {
+            return random.Next(1, numFaces + 1);
+        }
+    }
+}
---
+++ Lista2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Lista2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DadoVirtual dado6Faces = new DadoVirtual(6);
+            DadoVirtual dado8Faces = new DadoVirtual(8);
+            DadoVirtual dado12Faces = new DadoVirtual(12);
+
+            Console.WriteLine("Jogando o dado de 6 faces:");
+            for (int i = 0; i < 3; i++)
+            {
+                int resultado = dado6Faces.Rolar();
+                Console.WriteLine($"Resultado: {resultado}");
+            }
+
+            Console.WriteLine("\nJogando o dado de 8 faces:");
+            for (int i = 0; i < 3; i++)
+            {
+                int resultado = dado8Faces.Rolar();
+                Console.WriteLine($"Resultado: {resultado}");
+            }
+
+            Console.WriteLine("\nJogando o dado de 12 faces:");
+            for (int i = 0; i < 3; i++)
+            {
+                int resultado = dado12Faces.Rolar();
+                Console.WriteLine($"Resultado: {resultado}");
+            }
+        }
+    }
+}
