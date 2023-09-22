c60d2aa0888363c64c63667b478f109d004bbb7a
Allan Almeida <92676069+sonatou@users.noreply.github.com>
2023-09-21 22:00:33 -0300

    Lista 2 exercicio 2

---
+++ Lista2/Calculadora.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2
+{
+    internal class Calculadora
+    {
+
+        public int Soma(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Subtrai(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Multiplicar(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Divide(int x, int y)
+        {
+            return x / y;
+        }
+        
+    }
+}
